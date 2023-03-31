cd0e733b8d575161d6139d0777a2a7f671fd908b
Thehlastprogrammer <123794674+Thehlastprogrammer@users.noreply.github.com>
2023-03-31 16:21:40 -0600

    Create Examen3.java

---
+++ Examen3.java
@@ -0,0 +1,11 @@
+package com.mycompany.examen3;
+
+public class Examen3 {
+
+    public static void main(String[] args) {
+        Lector archivo = new Lector();
+        //173957
+        archivo.ejecutar(173957);
+        
+    }
+}
